66ee30fa1f83ccfb455f7c43ffba7de7bd8b2266
Amitav Roy <reachme@amitavroy.com>
2024-11-12 05:34:39 +0530

    Video 6 - Adding the code for login page

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+  email: null,
+  password: null
+})
+
+function submit() {
+  form.post('login')
+}
+</script>
+
+<template>
+  <div>
+    <p>Login page</p>
+
+    <pre>{{ form }}</pre>
+
+    <form @submit.prevent="submit">
+      <div class="mb-2">
+        <label for="email">Email address</label>
+        <input type="text" id="email" v-model="form.email" placeholder="Enter email">
+        <div class="text-red-300 text-sm" v-if="form.errors.email">{{ form.errors.email }}</div>
+      </div>
+
+      <div class="mb-2">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="form.password" placeholder="Enter password">
+        <div class="text-red-300 text-sm" v-if="form.errors.password">{{ form.errors.password }}</div>
+      </div>
+
+      <div class="mb-2">
+        <input type="submit" value="Login">
+      </div>
+    </form>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,15 @@ use Inertia\Inertia;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/profile', ProfileController::class)->name('profile');
+Route::inertia('login', 'Login');
+Route::post('login', function(Request $request) {
+    $postData = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required', 'min:6'],
+    ]);
+
+    return to_route('home');
+});
 Route::post('/test', function (Request $request) {
     logger($request->all());
     // return to_route('profile');
